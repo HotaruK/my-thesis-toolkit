ad4ae7c124a0b72211c07000391baef0da0f2a6b
Hotaru Komajo <30549239+HotaruK@users.noreply.github.com>
2023-10-03 10:59:05 -0700

    Update generatePoseImage.py

--- src/generatePoseImage/generatePoseImage.py
+++ src/generatePoseImage/generatePoseImage.py
@@ -10,6 +10,7 @@ mp_holistic = mp.solutions.holistic
 landmark_drawing_spec = mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=0, circle_radius=0)
 connection_drawing_spec = mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=1)
 num_workers = multiprocessing.cpu_count()
+mp_face_mesh = mp.solutions.face_mesh
 
 # landmark data for pose(body and arms)
 pose_landmarks = [mp_holistic.PoseLandmark.LEFT_SHOULDER,
@@ -28,6 +29,13 @@ pose_connections = [(mp_holistic.PoseLandmark.LEFT_SHOULDER, mp_holistic.PoseLan
                     (mp_holistic.PoseLandmark.LEFT_HIP, mp_holistic.PoseLandmark.RIGHT_HIP),
                     (mp_holistic.PoseLandmark.LEFT_SHOULDER, mp_holistic.PoseLandmark.LEFT_HIP),
                     (mp_holistic.PoseLandmark.RIGHT_SHOULDER, mp_holistic.PoseLandmark.RIGHT_HIP)]
+face_landmarks = [
+    [57, 13, 291, 14, 57],  # mouth
+    [70, 53, 52, 65, 55],  # left eyebrow
+    [285, 295, 282, 283, 300],  # right eyebrow
+    [468],  # left eye
+    [473],  # right eye
+]
 
 
 def __drawing_pose(image, results):
@@ -46,11 +54,27 @@ def __drawing_pose(image, results):
                  connection_drawing_spec.thickness)
 
 
+def __drawing_face(image, results):
+    for landmark_group in face_landmarks:
+        for i in range(len(landmark_group) - 1):
+            start_point = results.face_landmarks.landmark[landmark_group[i]]
+            end_point = results.face_landmarks.landmark[landmark_group[i + 1]]
+            start_loc = (int(start_point.x * image.shape[1]), int(start_point.y * image.shape[0]))
+            end_loc = (int(end_point.x * image.shape[1]), int(end_point.y * image.shape[0]))
+            cv2.line(image, start_loc, end_loc, connection_drawing_spec.color,
+                     connection_drawing_spec.thickness)
+        # Draw circles for the eyes
+        if len(landmark_group) == 1:
+            landmark_point = results.face_landmarks.landmark[landmark_group[0]]
+            loc = (int(landmark_point.x * image.shape[1]), int(landmark_point.y * image.shape[0]))
+            cv2.circle(image, loc, landmark_drawing_spec.circle_radius * 2, landmark_drawing_spec.color,
+                       landmark_drawing_spec.thickness)
+
 def _process_image(output_options: dict, image_file: str, output_name: str):
     image = cv2.imread(image_file)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    with mp_holistic.Holistic(static_image_mode=True) as holistic:
+    with mp_holistic.Holistic(static_image_mode=True, refine_face_landmarks=True) as holistic:
         results = holistic.process(image)
 
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -63,6 +87,9 @@ def _process_image(output_options: dict, image_file: str, output_name: str):
                                   landmark_drawing_spec, connection_drawing_spec)
         mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
                                   landmark_drawing_spec, connection_drawing_spec)
+
+    if output_options['face']:
+        __drawing_face(image, results)
 
     cv2.imwrite(output_name, image)
 
@@ -93,6 +120,7 @@ if __name__ == '__main__':
     option = {
         'hands': True,
         'pose': True,
+        'face': True,
     }
     work_dir = ''
     input_dir = work_dir + '/train'
