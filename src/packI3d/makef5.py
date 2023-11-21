0456e5fdf1a57fc11a02de0ea87f6ccf86432250
Hotaru Komajo <30549239+HotaruK@users.noreply.github.com>
2023-11-21 14:15:08 -0800

    Update makef5.py

--- src/packI3d/makef5.py
+++ src/packI3d/makef5.py
@@ -1,16 +1,28 @@
+import os
 import numpy as np
 
-file1 = 'path_to_your_file1.npz'
-file2 = 'path_to_your_file2.npz'
-output_dir = 'path_to_your_output_directory'
+def marge_tensor(filename, file_path1, file_path2, output1, output2):
+    data1 = np.load(file_path1).f.feature
+    data2 = np.load(file_path2).f.feature
 
-tensor1 = np.load(file1)['arr_0']
-tensor2 = np.load(file2)['arr_0']
+    # (2, frames, 1024)
+    new_tensor1 = np.stack((data1, data2))
+    # (1, frames, 2048)
+    new_tensor2 = np.concatenate((data1, data2), axis=2)
 
-tensor_combined_1 = np.concatenate((tensor1, tensor2), axis=0)  # (2,1024)
-tensor_combined_2 = np.concatenate((tensor1, tensor2), axis=1)  # (1,2048)
+    np.savez(f'{output1}/{filename}.npz', new_tensor1)
+    np.savez(f'{output2}/{filename}.npz', new_tensor2)
 
-np.savez(output_dir + '/combined_tensor_1.npz', tensor_combined_1)
-np.savez(output_dir + '/combined_tensor_2.npz', tensor_combined_2)
+def process(input_dir1, input_dir2, output_dir1, output_dir2):
+    filenames = os.listdir(input_dir1)
+    for filename in filenames:
+        file_path1 = os.path.join(input_dir1, filename)
+        file_path2 = os.path.join(input_dir2, filename)
+        marge_tensor(filename, file_path1, file_path2, output_dir1, output_dir2)
 
 if __name__ == '__main__':
+    input_dir1 = ''
+    input_dir2 = ''
+    output_dir1 = ''
+    output_dir2 = ''
+    process(input_dir1, input_dir2, output_dir1, output_dir2)
