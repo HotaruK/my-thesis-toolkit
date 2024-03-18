02cfddab57ed0bc3a556d795f6fbd730d4e2510e
hotaruk <tomoe2781@yahoo.co.jp>
2024-03-18 10:05:17 -0700

    Update makeMargedDataset.py

--- src/packI3d/makeMargedDataset.py
+++ src/packI3d/makeMargedDataset.py
@@ -9,7 +9,7 @@ TOTAL_MINIMIZE_LANDMARKS = 1208
 
 
 def marge_tensor(base_obj, additional_obj):
-    new_sign = torch.cat((additional_obj['sign'], base_obj['sign']), dim=2)
+    new_sign = torch.cat((additional_obj['sign'], base_obj['sign']), dim=1)
     base_obj['sign'] = new_sign
 
     return base_obj
@@ -22,7 +22,10 @@ def find_and_pop_name_by_object(objects, target_name):
     return None
 
 
-def _process_dataset(base_ds_path, output_dir, additional_dataset_path, type, prefix):
+def _process_dataset(base_ds_path, output_dir, additional_dataset_path, type, prefix, process='merge', key_name=None):
+    assert process in ['merge', 'add']
+    assert (process == 'add' and key_name)
+
     with gzip.open(base_ds_path, 'rb') as f:
         base_ds = pickle.load(f)
 
@@ -31,15 +34,17 @@ def _process_dataset(base_ds_path, output_dir, additional_dataset_path, type, pr
 
     progress_bar = tqdm(total=len(base_ds) + 1)
 
-    for k, d in base_ds.items():
-        base_ds[k] = marge_tensor(base_obj=d,
-                                  additional_obj=find_and_pop_name_by_object(additional_ds, d.get('name')))
+    if process == 'merge':
+        for k, d in enumerate(base_ds):
+            base_ds[k] = marge_tensor(base_obj=d,
+                                      additional_obj=find_and_pop_name_by_object(additional_ds, d.get('name')))
+    elif process == 'add':
+        for k, d in enumerate(base_ds):
+            base_ds[k][key_name] = find_and_pop_name_by_object(additional_ds, d.get('name'))
+
         progress_bar.update(1)
 
     del additional_ds
-
-    for i in base_ds:
-        assert i['sign'].shape[2] == TOTAL_MINIMIZE_LANDMARKS
 
     with gzip.open(os.path.join(output_dir, f'{prefix}.{type}.pkl.gz'), 'wb') as f:
         pickle.dump(base_ds, f)
